39caa012fc441e81ae2a6058aca42d40b12c01f9
eetu koljonen <eetu.koljonen@gmail.com>
2023-12-11 12:59:48 +0200

    minor fixes

--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: ekoljone <ekoljone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 16:09:18 by ekoljone          #+#    #+#             */
-/*   Updated: 2023/12/11 12:33:42 by ekoljone         ###   ########.fr       */
+/*   Updated: 2023/12/11 12:58:07 by ekoljone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,15 @@
 
 #include <iostream>
 
-template< typename T >
+template<typename T>
 void print_element( T& x )
 {
   std::cout << x << std::endl;
   return;
 }
 
-template <typename T>
-void	iter(T *addr, size_t len, void (*f)(T const &))
-{
-	for (size_t i = 0; i < len; i++)
-	{
-		f(addr[i]);
-	}
-}
-
-template <typename T>
-void	iter(T *addr, size_t len, void (*f)(T &))
+template <typename T, typename T1>
+void	iter(T *addr, size_t len, void (*f)(T1 &))
 {
 	for (size_t i = 0; i < len; i++)
 	{
